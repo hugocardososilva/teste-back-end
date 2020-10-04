231003fd96c2076b142484646b5731dfac0c5cfa
Hugo Silva <hugocardososilva@gmail.com>
2020-10-04 18:56:33 -0300

    test(user): add test user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  context "Validations" do
+    it 'is avalid user create' do
+      expect(user).to be_valid
+    end
+       it "has a unique email" do
+      some_user = FactoryBot.build(:user, email: user.email)
+      expect(some_user).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      some_user = FactoryBot.build(:user, password: nil)
+      expect(some_user).to_not be_valid
+    end
+
+    it "is not valid without an email" do
+      some_user = FactoryBot.build(:user, email: nil)
+      expect(some_user).to_not be_valid
+    end
+
+  end
+
 end
