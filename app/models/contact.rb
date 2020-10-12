f3652e99efbd06dadcc9ca062728b2f1c7dc5398
Hugo Silva <hugocardososilva@gmail.com>
2020-10-11 22:46:48 -0300

    feat(contact): add callback when contact create to send email to admin

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,12 @@
 class Contact < ApplicationRecord
+  after_create :send_mail
+
+  def to_s
+    "#{name} - #{email}"
+  end
+  private
+
+  def send_mail
+    ContactMailer.with(contact: self).catch_contact.deliver_later
+  end
 end
